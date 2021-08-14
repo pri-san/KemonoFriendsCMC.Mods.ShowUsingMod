81a09e2d82df6138773c4acaaf59396c3b004031
pri-san <88888437+pri-san@users.noreply.github.com>
2021-08-14 18:42:44 +0900

    プロジェクト追加

---
+++ KemonoFriendsCMC.Mods.ShowUsingMod/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("KemonoFriendsCMC.Mods.ShowUsingMod")]
+[assembly: AssemblyDescription("This is KemonoFriendsCMC MOD")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("pri-san")]
+[assembly: AssemblyProduct("KemonoFriendsCMC.Mods.ShowUsingMod")]
+[assembly: AssemblyCopyright("Copyright © 2021 pri-san")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("69f51a5c-b081-4b97-b8e3-e29abfa344ed")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
